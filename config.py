36ae3ad1a1be664be0c19742198545fa5c4ce5f4
Morris Chou <111498626+morris-c-123@users.noreply.github.com>
2022-09-05 23:40:48 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-"""api and password for app.py"""
-
-csv_locaiton = r""
-# line api
-line_api = "CcHlR5p4GporOQ8PYrjXeL647m2OPq0Y14BOrtx1toY"
-
-# FB log in
-my_email = "morris.chou@meltwater.com"
-my_password = "@!b2c3d4e5"
